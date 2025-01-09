9a20ea6d9fa0ed22fc2b631bca7c71e85434768b
d2theadi04 <paphada412@gmail.com>
2025-01-09 16:02:07 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './Log_and_Regist.css'
-import App from './LogIn_and_Regist.jsx'
+import './index.css'
+import App from './Info_Regist_Page.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
